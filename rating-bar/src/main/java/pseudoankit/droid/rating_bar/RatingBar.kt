e9243f926cc7830df5430f26adb7c031b8d1b309
Ankit Kumar <kumarankit12353@gmail.com>
2022-09-17 13:35:19 +0530

    icons updated on drag event

--- rating-bar/src/main/java/pseudoankit/droid/rating_bar/RatingBar.kt
+++ rating-bar/src/main/java/pseudoankit/droid/rating_bar/RatingBar.kt
@@ -1,46 +1,58 @@
 package pseudoankit.droid.rating_bar
 
+import android.util.Log
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.IntSize
+import kotlin.math.roundToInt
+
+const val TAG = "dRatingBar"
 
 @Composable
 fun RatingBar(
     config: RatingBarConfig = RatingBarConfig()
 ) {
     var activeIconIndex by remember { mutableStateOf(0) }
-    var rowWidth by remember { mutableStateOf(0) }
+    var iconWidth by remember { mutableStateOf(0) }
+    var finalDragAmt by remember { mutableStateOf(0f) }
 
-    Row(
+    Box(
         modifier = Modifier
-            .createRatingBarModifier(config) { rowWidth = it.width }
-            .pointerInput(Unit) {
-                detectHorizontalDragGestures(
-                    onDragEnd = {},
-                    onDragCancel = {},
-                    onDragStart = {},
-                    onHorizontalDrag = { _, dragAmount: Float ->
-                        //Log.d("RatingBar", "RatingBar: $dragAmount")
-                    }
-                )
-            },
-        horizontalArrangement = Arrangement.Center
+            .height(config.iconConfig.height)
     ) {
-        DrawRatingBarIcons(config, activeIconIndex) { activeIconIndex = it }
+        Row(
+            modifier = Modifier
+                .onSizeChanged { iconWidth = it.width / config.iconCount }
+                .pointerInput(Unit) {
+                    detectHorizontalDragGestures(
+                        onDragCancel = {},
+                        onDragEnd = {},
+                        onDragStart = {
+                            finalDragAmt = it.x
+                        },
+                        onHorizontalDrag = { _, dragAmount ->
+
+                            finalDragAmt += dragAmount
+                            Log.d("$TAG Dragging", "DragAmount: $finalDragAmt")
+
+                            val iconIdx = (finalDragAmt / iconWidth).roundToInt()
+                            Log.d(TAG, "dragIconIdx: $iconIdx")
+
+                            if (iconIdx != activeIconIndex) {
+                                activeIconIndex = iconIdx
+                            }
+                        }
+                    )
+                },
+            horizontalArrangement = Arrangement.Center
+        ) {
+            DrawRatingBarIcons(config, activeIconIndex) { activeIconIndex = it }
+        }
     }
 }
-
-private fun Modifier.createRatingBarModifier(
-    config: RatingBarConfig,
-    onSizeChanged: (IntSize) -> Unit
-) = this
-    .height(config.iconConfig.height)
-    .padding(paddingValues = config.padding)
-    .onSizeChanged(onSizeChanged)
